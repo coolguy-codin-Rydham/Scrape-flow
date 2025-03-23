7a7ad6e6dd8e9a5c5e6ef3c6a027fd430a86a03d
RYdham <rydhampreetsingh.gindra@gmail.com>
2025-03-23 13:08:05 +0530

    Clerk Authentication Complete, Protected Routes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import DesktopSidebar from "@/components/Sidebar";
 import { Separator } from "../../components/ui/separator";
 import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import { ModeToggle } from "@/components/ThemeModeToggle";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,8 +11,11 @@ const layout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
-          <div className="gap-1 flex items-center ">
+          <div className="gap-3 flex items-center ">
             <ModeToggle />
+            <SignedIn>
+              <UserButton/>
+            </SignedIn>
           </div>
         </header>
         <Separator />
